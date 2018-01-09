24504c84adf9b009ea7f8498901727faa29f4c4f
Joseph Crail <jbcrail@gmail.com>
2018-01-09 14:35:51 -0800

    Deploy package only on tagged commit

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requires = open('requirements.txt').read().strip().split('\n')
 
 setup(
     name='intake-pcap',
-    version='0.1',
+    version='0.0.1',
     description='Intake PCAP plugin',
     url='https://github.com/ContinuumIO/intake-pcap',
     maintainer='Joseph Crail',
