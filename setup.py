ec653fc6c6f74ecc91c3d8e1fabaaede18bc37bb
Joseph Crail <jbcrail@gmail.com>
2018-03-14 08:32:52 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requires = open('requirements.txt').read().strip().split('\n')
 
 setup(
     name='intake-pcap',
-    version='0.0.6',
+    version='0.0.7',
     description='Intake PCAP plugin',
     url='https://github.com/ContinuumIO/intake-pcap',
     maintainer='Joseph Crail',
